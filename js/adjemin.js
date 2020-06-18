918b1227c1970420389844635a6bd7d89145a9fa
Amane Hosanna <amanehosanna@gmail.com>
2020-06-18 19:47:29 +0000

    terminated

---
+++ js/adjemin.js
@@ -0,0 +1,32 @@
+
+var slideIndex = 1;
+showSlides(slideIndex);
+alert("ready");
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide-item");
+    var dots = document.getElementsByClassName("circle-indicator");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    setTimeout(() => {
+        showSlides(1);
+    }, 1000);
+}
